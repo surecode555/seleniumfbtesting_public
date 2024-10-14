944df6c508a0e730c8f2bcd12b30328513bdace0
Suresh <suresh@email.com>
2024-10-14 11:00:04 -0400

    my first commit

---
+++ src/test/java/org/example/Test1_Facebook_Login.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1_Facebook_Login {
+
+    @Test
+    public void login()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("testuser@devopsxyz.com");
+        driver.findElement(By.xpath("//input[@name='pass']")).sendKeys("abc@#12345");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        boolean displayed = driver.findElement(By.xpath("//a[contains(text(),'Find your account and log in.')]")).isDisplayed();
+        Assert.assertTrue("Expected message is true",displayed);
+        driver.quit();
+    }
+}
