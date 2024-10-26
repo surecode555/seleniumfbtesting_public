5118fd47e5a8da387e9e0a02cccbe4706f2fd20b
Suresh <suresh@email.com>
2024-10-25 21:11:31 -0400

    Updated chrome options

--- src/test/java/org/example/Test1_Facebook_Login.java
+++ src/test/java/org/example/Test1_Facebook_Login.java
@@ -12,17 +12,18 @@ public class Test1_Facebook_Login {
     @Test
     public void login()
     {
-        WebDriver driver = new ChromeDriver();
-        ChromeOptions options=new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
+        ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.facebook.com/");
         driver.findElement(By.xpath("//input[@name='email']")).sendKeys("testuser@devopsxyz.com");
         driver.findElement(By.xpath("//input[@name='pass']")).sendKeys("abc@#12345");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         boolean displayed = driver.findElement(By.xpath("//a[contains(text(),'Find your account and log in.')]")).isDisplayed();
         Assert.assertTrue("Expected message is true",displayed);
+        System.out.println("Test script executed successfully");
         driver.quit();
+        System.out.println("Webdriver is closed successfully");
     }
 }
