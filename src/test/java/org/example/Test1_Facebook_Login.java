2e2352466e44fc5821d37cb2fa325d7309399baf
Suresh <suresh@email.com>
2024-10-25 21:24:57 -0400

    added wait time

--- src/test/java/org/example/Test1_Facebook_Login.java
+++ src/test/java/org/example/Test1_Facebook_Login.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
 public class Test1_Facebook_Login {
 
     @Test
@@ -20,6 +22,7 @@ public class Test1_Facebook_Login {
         driver.findElement(By.xpath("//input[@name='email']")).sendKeys("testuser@devopsxyz.com");
         driver.findElement(By.xpath("//input[@name='pass']")).sendKeys("abc@#12345");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
         boolean displayed = driver.findElement(By.xpath("//a[contains(text(),'Find your account and log in.')]")).isDisplayed();
         Assert.assertTrue("Expected message is true",displayed);
         System.out.println("Test script executed successfully");
